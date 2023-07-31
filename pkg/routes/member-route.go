c56bc7b4a3a44cb6d95c59195b23fd50335957a4
martina <you@example.com>
2023-07-31 06:28:58 +0100

    Created the routes for the API call

---
+++ pkg/routes/member-route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Ezinnem/workers/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var MembersRoutes = func(router *mux.Router) {
+	router.HandleFunc("/member", controllers.CreateMember).Methods("POST")
+	router.HandleFunc("/member", controllers.GetMember).Methods("GET")
+	router.HandleFunc("/member/{memberId}", controllers.GetMemberById).Methods("GET")
+	router.HandleFunc("/member/{memberId}", controllers.UpdateMember).Methods("PUT")
+	router.HandleFunc("/member/{memberId}", controllers.DeleteMember).Methods("DELETE")
+}
