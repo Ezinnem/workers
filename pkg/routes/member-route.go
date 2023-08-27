b38584b17adecd2b45fa40b81a56c346e2b583f3
Ezinne <nnamani.ezinne@gmail.com>
2023-08-27 12:57:15 +0100

    feat: Updated the Routes for the member routes

--- pkg/routes/member-route.go
+++ pkg/routes/member-route.go
@@ -7,7 +7,7 @@ import (
 
 var MembersRoutes = func(router *mux.Router) {
 	router.HandleFunc("/member", controllers.CreateMember).Methods("POST")
-	router.HandleFunc("/member", controllers.GetMember).Methods("GET")
+	router.HandleFunc("/members", controllers.GetAllMembers).Methods("GET")
 	router.HandleFunc("/member/{memberId}", controllers.GetMemberById).Methods("GET")
 	router.HandleFunc("/member/{memberId}", controllers.UpdateMember).Methods("PUT")
 	router.HandleFunc("/member/{memberId}", controllers.DeleteMember).Methods("DELETE")
